c96e69fb912890ff1ee4f15b226ee2ba20f21b69
MarkGhebrial <mghebrialjr@icloud.com>
2022-12-07 14:15:16 -0800

    Day 6 part 2

---
+++ day6/part2.rs
@@ -0,0 +1,21 @@
+use std::fs;
+
+fn main() {
+    let transmission = fs::read_to_string("input.txt").unwrap();
+
+    let mut index = 0;
+
+    for i in 0..transmission.len() {
+        let mut has_double = false;        
+        for c in transmission[i..i+14].chars() {
+            if transmission[i..i+14].matches(c).collect::<Vec<&str>>().len() > 1 {
+                has_double = true;
+                break
+            }
+        }
+        if !has_double { break; }
+        index += 1;
+    }
+
+    println!("{}", index + 14);
+}
