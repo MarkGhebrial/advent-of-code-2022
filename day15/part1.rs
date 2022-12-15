46de6686d337d6d2dfd8b8c9fccab6f02bf192a5
MarkGhebrial <mghebrialjr@icloud.com>
2022-12-15 14:30:04 -0800

    Day 15 part 1

--- day15/part1.rs
+++ day15/part1.rs
@@ -1,90 +1,73 @@
 use std::fs;
 
-#[derive(PartialEq)]
-enum CellState {
-    Searched,
-    Unsearched,
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: isize,
+    y: isize,
 }
 
-struct Grid(Vec<Vec<CellState>>);
-
-impl Grid {
-    fn new() -> Self {
-        let mut v = Vec::with_capacity(50000);
-        for _ in 0..50000 {
-            let mut v1 = Vec::with_capacity(50000);
-            for _ in 0..50000 {
-                v1.push(CellState::Unsearched);
-            }
-            v.push(v1);
-        }
-
-        Grid(v)
-    }
-
-    fn get_mut(&mut self, x: isize, y: isize) -> &mut CellState {
-        &mut self.0[(y+10000) as usize][(x+10000) as usize]
-    }
-
-    fn get(&self, x: isize, y: isize) -> &CellState {
-        &self.0[(y+10000) as usize][(x+10000) as usize]
-    }
-
-    fn len_y(&self) -> usize {
-        self.0.len()
-    }
-
-    fn len_x(&self) -> usize {
-        self.0[0].len()
-    }
-
-    /*fn print(&self) {
-        for v in self.0.iter() {
-            for c in v {
-                print!("{}", c);
-            }
-            println!("");
-        }
-    }*/
+#[derive(Debug)]
+struct Sensor {
+    position: Point,
+    beacon: Point,
 }
 
-fn main() {
-    let input = fs::read_to_string("test.txt").unwrap();
-
-    println!("Allocating memory...");
-    let mut grid = Grid::new();
-    println!("Allocated");
-
-    for line in input.trim().split("\n") {
-        println!("line: {}", line);
-        
+impl Sensor {
+    fn parse_from_str(line: &str) -> Self {
         let line = line.trim().split(" ").collect::<Vec<&str>>();
-        let sensor_x = line[2][2..line[2].len() - 1].parse::<isize>().unwrap();
-        let sensor_y = line[3][2..line[3].len() - 1].parse::<isize>().unwrap();
+        let x = line[2][2..line[2].len() - 1].parse::<isize>().unwrap();
+        let y = line[3][2..line[3].len() - 1].parse::<isize>().unwrap();
 
         let beacon_x = line[8][2..line[8].len() - 1].parse::<isize>().unwrap();
         let beacon_y = line[9][2..line[9].len()].parse::<isize>().unwrap();
 
-        println!("S: ({}, {}) B: ({}, {})", sensor_x, sensor_y, beacon_x, beacon_y);
-
-        let dist = (sensor_x - beacon_x).abs() + (sensor_y - beacon_y).abs();
-
-        println!("Dist: {dist}");
-
-        for (i, x) in (sensor_x-dist..sensor_x+dist).enumerate() {
-            //println!("x = {}", x);
-            let i = i as isize;
-            for y in x-i..=x+i {
-                println!("({}, {})", x, y);
-                *grid.get_mut(x, y) = CellState::Searched;
-            }
+        Self {
+            position: Point {
+                x,
+                y,
+            },
+            beacon: Point {
+                x: beacon_x,
+                y: beacon_y,
+            },
         }
     }
 
+    fn dist_to(&self, p: &Point) -> isize {
+        (self.position.x - p.x).abs() + (self.position.y - p.y).abs()
+    }
+
+    fn dist_to_beacon(&self) -> isize {
+        self.dist_to(&self.beacon)
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let mut sensors: Vec<Sensor> = Vec::new();
+
+    for line in input.trim().split("\n") {
+        
+        let sensor = Sensor::parse_from_str(&line.trim());
+        sensors.push(sensor);
+    }
+
     let mut count = 0;
-    for x in 0..grid.len_x() {
-        if *grid.get(x as isize, 2000000) == CellState::Unsearched {
-            count += 1;
+    for x in -7000000..=12000000 {
+        let point = Point {
+            x,
+            y: 2000000,
+        };
+        'fdsa: for sensor in sensors.iter() {
+            if point == sensor.beacon {
+                continue;
+            }
+            if sensor.dist_to(&point) <= sensor.dist_to_beacon() || point == sensor.position {
+                println!("To point: {}, to Beacon: {}", sensor.dist_to(&point), sensor.dist_to_beacon());
+                count += 1;
+                break 'fdsa;
+            }
         }
     }
 
